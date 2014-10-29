279a82f71a8ea2e8ccb5d19b9d5d816da506d2eb
Edward Seim <sirseim@gmail.com>
2014-10-28 21:51:10 -0700

    WordSort v4 complete
    Code overhaul and cleanup. Correctly differentiates delimiters and non
    letters.

--- WordSort/WordSort.java
+++ WordSort/WordSort.java
@@ -1,91 +1,50 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.lang.Character;
+import java.lang.StringBuilder;
 
 public class WordSort {
-	public static void main (String[] args) {
-		// Setup reader
+	public static void main (String[] args) throws Exception{
+		// Setup
 		java.io.BufferedReader stdIn = new java.io.BufferedReader ( new java.io.InputStreamReader ( System.in ) );
-		ArrayList <String> lines = new ArrayList<String>();
-		boolean isSensitive = false;
-		if (args.length > 0 && args[0].equals("-sensitive")) {
-			isSensitive = true;
-		}
-
-		// Capture System Input
-		String s;
-		try {
-			s = stdIn.readLine();
-		} catch (Exception e) {
-			throw new IllegalArgumentException("Failed readLine");
-		}
-		while ( s != null ) {
-			lines.add(s);
-			try {
-				s = stdIn.readLine();
-			} catch (Exception e) {
-				throw new IllegalArgumentException("Failed continued readLine");
-			}
-		}
-
-		// Separate out words
 		ArrayList <String> words = new ArrayList<String>();
-		boolean isDone = false;
-		int start, end, index;
-		boolean isFound;
-		String currentLine;
-		for (int i = 0; i < lines.size(); i++) {
-			start = 0;
-			end = 0;
-			index = 0;
-			isDone = false;
-			currentLine = lines.get(i);
-			if (currentLine.length() == 0) {
-				isDone = true;
-			}
-			while (!isDone) {
-				isFound = Character.isLowerCase(currentLine.charAt(index)) || Character.isUpperCase(currentLine.charAt(index));
-				if (isFound) {
-					end++;
-				} else {
-					if (start <= end - 1) {
-						if (isSensitive) {
-							words.add(currentLine.substring(start, end));
-						} else {
-							words.add(currentLine.substring(start, end).toUpperCase());
-						}
-					}
-					end++;
-					start = end;
+		String[] tLine;
+		StringBuilder tWord;
+		boolean isSensitive = (args.length > 0 && args[0].equals("-sensitive"));
+
+		// Capture System Input & Seprate out words
+		String s = stdIn.readLine();
+		while ( s != null ) {
+			tLine = s.split("(;|:|,|-| )");
+			for (int i = 0; i < tLine.length; i++) {
+				tWord = new StringBuilder(tLine[i]);
+				for (int j = 0; j < tWord.length(); j++) {
+					if (!Character.isLetter(tWord.charAt(j))) tWord.deleteCharAt(j--);
 				}
-				index++;
-				if (index == currentLine.length()){
-					if (start <= end - 1) {
-						if (isSensitive) {
-							words.add(currentLine.substring(start, end));
-						} else {
-							words.add(currentLine.substring(start, end).toUpperCase());
-						}
+				if (tWord.length() != 0) {
+					if (isSensitive) {
+						words.add(tWord.toString());
+					} else {
+						words.add(tWord.toString().toUpperCase());
 					}
-					isDone = true;
 				}
 			}
+			s = stdIn.readLine();
 		}
 
 		// Sorting
-		Object[] sortingArray = words.toArray();
-		Arrays.sort(sortingArray);
+		Collections.sort(words);
 
-		// Counting and Printing
+		// Counting & Printing
 		int counter = 1;
-		for (int i = 1; i < sortingArray.length; i++) {
-			if (sortingArray[i].equals(sortingArray[i - 1])) {
+		for (int i = 1; i < words.size(); i++) {
+			if (words.get(i).equals(words.get(i - 1))) {
 				counter++;
 			} else {
-				System.out.println(sortingArray[i - 1] + " " + counter);
+				System.out.println(words.get(i - 1) + " " + counter);
 				counter = 1;
 			}
 		}
-		System.out.println(sortingArray[sortingArray.length - 1] + " " + counter);
+		System.out.println(words.get(words.size() - 1) + " " + counter);
 	}
 }
