37dd269f5d43287f25e0620d7f5a7584a2b50359
Edward Seim <sirseim@gmail.com>
2014-11-12 21:42:05 -0800

    GitHub Bug
    Who knows why this commit won’t go away

--- WordSort/WordSort.java
+++ WordSort/WordSort.java
@@ -4,47 +4,47 @@ import java.lang.Character;
 import java.lang.StringBuilder;
 
 public class WordSort {
-	public static void main (String[] args) throws Exception{
-		// Setup
-		java.io.BufferedReader stdIn = new java.io.BufferedReader ( new java.io.InputStreamReader ( System.in ) );
-		ArrayList <String> words = new ArrayList<String>();
-		String[] tLine;
-		StringBuilder tWord;
-		boolean isSensitive = (args.length > 0 && args[0].equals("-sensitive"));
+    public static void main (String[] args) throws Exception{
+        // Setup
+        java.io.BufferedReader stdIn = new java.io.BufferedReader ( new java.io.InputStreamReader ( System.in ) );
+        ArrayList <String> words = new ArrayList<String>();
+        String[] tLine;
+        StringBuilder tWord;
+        boolean isSensitive = (args.length > 0 && args[0].equals("-sensitive"));
 
-		// Capture System Input & Seprate out words
-		String s = stdIn.readLine();
-		while ( s != null ) {
-			tLine = s.split("(;|:|,|-| )");
-			for (int i = 0; i < tLine.length; i++) {
-				tWord = new StringBuilder(tLine[i]);
-				for (int j = 0; j < tWord.length(); j++) {
-					if (!Character.isLetter(tWord.charAt(j))) tWord.deleteCharAt(j--);
-				}
-				if (tWord.length() != 0) {
-					if (isSensitive) {
-						words.add(tWord.toString());
-					} else {
-						words.add(tWord.toString().toUpperCase());
-					}
-				}
-			}
-			s = stdIn.readLine();
-		}
+        // Capture System Input & Seprate out words
+        String s = stdIn.readLine();
+        while ( s != null ) {
+            tLine = s.split("(;|:|,|-| )");
+            for (int i = 0; i < tLine.length; i++) {
+                tWord = new StringBuilder(tLine[i]);
+                for (int j = 0; j < tWord.length(); j++) {
+                    if (!Character.isLetter(tWord.charAt(j))) tWord.deleteCharAt(j--);
+                }
+                if (tWord.length() != 0) {
+                    if (isSensitive) {
+                        words.add(tWord.toString());
+                    } else {
+                        words.add(tWord.toString().toUpperCase());
+                    }
+                }
+            }
+            s = stdIn.readLine();
+        }
 
-		// Sorting
-		Collections.sort(words);
+        // Sorting
+        Collections.sort(words);
 
-		// Counting & Printing
-		int counter = 1;
-		for (int i = 1; i < words.size(); i++) {
-			if (words.get(i).equals(words.get(i - 1))) {
-				counter++;
-			} else {
-				System.out.println(words.get(i - 1) + " " + counter);
-				counter = 1;
-			}
-		}
-		System.out.println(words.get(words.size() - 1) + " " + counter);
-	}
+        // Counting & Printing
+        int counter = 1;
+        for (int i = 1; i < words.size(); i++) {
+            if (words.get(i).equals(words.get(i - 1))) {
+                counter++;
+            } else {
+                System.out.println(words.get(i - 1) + " " + counter);
+                counter = 1;
+            }
+        }
+        System.out.println(words.get(words.size() - 1) + " " + counter);
+    }
 }
